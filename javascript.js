84bbc3c8812a035713e5886492563dd8a97b0d80
OPZJazer <chawitsomkeate@gmail.com>
2025-03-23 15:50:17 +0700

    add revert console-log

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 console.log("Hello world");
 console.log("Hello github");
-
+console.log("TRYING OUT REVERT")
 
 // document.getElementById("button1").onclick = function(){
 //     console.log("hello world")
