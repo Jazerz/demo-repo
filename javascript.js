3b1eb87a30464e07f8fc8d667060ef1496279091
OPZJazer <chawitsomkeate@gmail.com>
2025-03-23 15:59:45 +0700

    save the day

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 console.log("Hello world");
 console.log("Hello github");
 
+//implement urgent fix
+
 
 // document.getElementById("button1").onclick = function(){
 //     console.log("hello world")
